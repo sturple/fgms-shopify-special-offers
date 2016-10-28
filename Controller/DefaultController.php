7c34104a1836b7116fbd1f1624de864868efdfdb
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-10-27 23:28:41 -0700

    Manual Merge - Shawn's Initial Commit
    Shawn and I started this repository with different root commits.  I'm bringing everything he committed in his initial commit into my branch and then overwriting his branch.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -10,4 +10,19 @@ class DefaultController extends Controller
     {
         return $this->render('FgmsSpecialOffersBundle:Default:index.html.twig');
     }
+
+    public function oauthAction()
+    {
+        $this->service = $this->container->get('fg.shopify');
+        $this->shopify = $this->service->get_shopify('FgmsSpecialOffersBundle');
+        // this means that good to go
+        if (!empty($this->shopify)) {
+            $settings = $this->service->get_settings();
+            $settings['page']['title'] = 'Special Offers';
+            $settings['page']['data'] = 'This is Shopify Special offers';
+            return $this->render('FgmsSpecialOffersBundle:Default:index.html.twig', $settings);
+        } else {
+            return $this->service->add_new_store();
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,3 +1,44 @@
+# Shopify Special Offers Bundle (Embed)
+
+## Installation
+
+** Install With Composer **
+
+```json
+{
+   "require": {
+       "sturple/fgms-shopify-special-offers": "dev-master"
+   }
+}
+
+```
+
+and then execute
+
+```json
+$ composer update
+```
+
+
+## Configuration
+
+**Add to ```app/AppKernel.php``` file**
+
+```php
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = [
+            ...
+             new Fgms\SpecialOffersBundle\FgmsSpecialOffersBundle();
+        ]
+    }
+}
+
+```
+
 ## Requirements
 
 64 bit PHP is required.  To check if you're running 64 bit PHP run the following code:
